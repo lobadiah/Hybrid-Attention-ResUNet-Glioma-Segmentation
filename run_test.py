c722494e4171ab5b692c6f90721eecf091c02d76
Louis Adamu Obadiah <louisobadiah@gmail.com>
2026-03-06 11:17:00 +0300

    Add run_test.py to test all models

---
+++ run_test.py
@@ -0,0 +1,17 @@
+# Run Test
+
+# This script is intended to test all models in the Hybrid-Attention-ResUNet-Glioma-Segmentation project.
+
+def test_all_models():
+    models = ['Model1', 'Model2', 'Model3']  # List your models here
+
+    for model in models:
+        print(f'Testing {model}...')
+        # Load model
+        # Run tests
+        # Log results
+
+    print('All models tested successfully.')
+
+if __name__ == '__main__':
+    test_all_models()
